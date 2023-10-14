41b880d787a7ab0479e54bebf5f77858a97931a6
yukgakmon <69315573+horaemong@users.noreply.github.com>
2023-10-15 02:48:18 +0900

    use state

--- src/app/list/page.js
+++ src/app/list/page.js
@@ -1,5 +1,8 @@
+"use client";
+import React, { useEffect, useState } from "react";
 export default function List() {
   let products = ["Tomato", "Pasta", "Coconut"];
+  let [volume, setVolume] = useState([0, 0, 0]);
   return (
     <div>
       <h2>Products</h2>
@@ -7,6 +10,25 @@ export default function List() {
         return (
           <div className="food" key={index}>
             <h4>{product} $40</h4>
+            <span> {volume[index]} </span>
+            <button
+              onClick={() => {
+                let newVolume = [...volume];
+                newVolume[index]++;
+                setVolume(newVolume);
+              }}
+            >
+              +
+            </button>
+            <button
+              onClick={() => {
+                let newVolume = [...volume];
+                newVolume[index]--;
+                setVolume(newVolume);
+              }}
+            >
+              -
+            </button>
           </div>
         );
       })}
