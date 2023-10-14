683bea8935ecb71e244e092505b853967d3af2b1
yukgakmon <69315573+horaemong@users.noreply.github.com>
2023-10-15 01:42:23 +0900

    component_props

--- src/app/cart/page.js
+++ src/app/cart/page.js
@@ -1,17 +1,28 @@
+import hello from "./hello.js";
 export default function Cart() {
+  let cart = ["Tomato", "Pasta", "Coconut"];
   return (
     <div>
+      {hello()}
+      <p>-------------------</p>
       <h4 className="title">Cart</h4>
-      <div className="cart-item">
-        <p>상품명</p>
-        <p>$40</p>
-        <p>1개</p>
-      </div>
-      <div className="cart-item">
-        <p>상품명</p>
-        <p>$40</p>
-        <p>1개</p>
-      </div>
+      <CartItem item={cart[0]}></CartItem>
+      <CartItem item={cart[1]}></CartItem>
     </div>
   );
+}
+
+function CartItem(props) {
+  return (
+    <div className="cart-item">
+      <p>{props.item}</p>
+      <p>$40</p>
+      <p>1개</p>
+      <Btn color="blue"></Btn>
+    </div>
+  );
+}
+
+function Btn(props) {
+  return <button style={{ background: props.color }}>버튼임</button>;
 }
